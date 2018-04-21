d340b1d90ba4940856d4832fe761e64e387bd370
zhangwei <cdzhangw@gmail.com>
2018-04-21 22:39:35 +0800

    cleanup index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,25 +20,6 @@ export default {
     this.$router.push({ name: 'messages', params: { idOrName: keyStore.getAddress() }});
   },
   methods: {
-    async sendMessage() {
-      var message = prompt('Message', '');
-      if (message==undefined) return;
-console.log(keystore.getPrivateKey().toPublicKey())
-      var transaction = await messageStore.generateMessageTransaction({hexString:transactionEncoder.toHex(message), publickKey:keystore.getPrivateKey().toPublicKey()});
-      console.log(transaction.toString());
-/*
-      var result = await blockchain.broadcast(transaction);
-      console.log(result);
-			setTimeout(()=>{
-      this.messages = await messageStore.getMessages(keystore.getAddress()); 
-			}, 5*1000);
-*/
-    },
-		async test() {
-      this.messages = await messageStore.getMessages(keystore.getAddress());
-      console.log(this.messages);
-      
-    }
   }
 }
 </script>

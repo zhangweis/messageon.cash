2edc75874ca494305d3248a8386f96657d348c39
zhangwei <cdzhangw@gmail.com>
2018-04-21 22:43:01 +0800

    extract host and address handling

--- src/services/blockchain.js
+++ src/services/blockchain.js
@@ -1,32 +1,46 @@
 import {PrivateKey, Transaction, Script, PublicKey, Output, Opcode} from 'bitcore-lib-cash';
 import * as pad from 'pad';
 import * as bitcore from 'bitcore-lib-cash';
+import * as bchaddrjs from 'bchaddrjs';
 class Blockchain {
-	async getAddress(address) {
-    const res = await fetch(`https://bch-insight.bitpay.com/api/addr/${address}`);
+  getUrl(u) {
+    return 'https://cashexplorer.bitcoin.com/api/'+u;
+  }
+  toHostAddress(address) {
+return bchaddrjs.toLegacyAddress(address);
+    return address;
+  }
+  async getAddress(addr) {
+    var address = this.toHostAddress(addr);
+    const res = await fetch(this.getUrl(`addr/${address}`));
     let addrObj = await res.json();
 		return addrObj;
 	}
-  async getUtxos(address) {
-    const utxo_res = await fetch(`https://bch-insight.bitpay.com/api/addr/${address}/utxo`);
+  toCashAddress(address) {     
+    return bchaddrjs.toCashAddress(address).split(':')[1];
+  }
+  async getUtxos(addr) {
+    var address = this.toHostAddress(addr);
+    const utxo_res = await fetch(this.getUrl(`addr/${address}/utxo`));
     let utxos = await utxo_res.json();
     var toRemove = [];
     for (var utxo of utxos) {
-    	let tx = await (await fetch('https://bch-insight.bitpay.com/api/tx/'+utxo.txid)).json();
+      utxo.address = this.toCashAddress(utxo.address);
+    	let tx = await (await fetch(this.getUrl('tx/'+utxo.txid))).json();
     	toRemove = toRemove.concat(tx.vin);
     }
     utxos = utxos.filter(utxo=>!toRemove.find(remove=>utxo.txid==remove.txid&&utxo.vout==remove.vout));
     return utxos;
   }
   async getTx(txid) {
-    return await (await fetch(`https://bch-insight.bitpay.com/api/tx/${txid}`)).json();
+    return await (await fetch(this.getUrl(`tx/${txid}`))).json();
   }
   async broadcast(transaction) {
     if (localStorage.debug) {
     		console.log('broadcasting ', transaction.toString());
     		return {};
     }
-    var res = await fetch(`https://bch-insight.bitpay.com/api/tx/send`, {
+    var res = await fetch(this.getUrl(`tx/send`), {
       body: JSON.stringify({rawtx:transaction.toString()}),
       method: 'POST',
       headers: {
